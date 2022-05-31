8806ae2d9a2b903c692b9f8fbd0a8b5636826d67
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-30 18:15:55 -0700

    didn't solve but progress in JS

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,6 +1,34 @@
-balanceParens = (str) =>
-{
+const balanceParens = (str) => {
+    let regexOne = /[^A-Z0-9)(]/gi;
+    let regexTwo = /[)(]/
+    let answerArr = [];
+    str = str.replace(regexOne, "");
+    let leftCount = 0;
+    let rightCount = 0;
+    
+  if (str.includes("(") === true && str.includes(")") === true) {
 
+    for (let i =0; i<str.length; i++) {
+      if ((str[i] === "(") && rightCount == leftCount) {
+        answerArr.push(str[i]);
+        leftCount++;
+      } else if (str[i] === ")") {
+        rightCount++;
+         if (answerArr.includes("(") && (rightCount == leftCount))
+        answerArr.push(str[i])
+          rightCount++;
+        } else {
+          answerArr.push(str[i]);
+        }
+      }
+  }else {
+  
+return answerArr;
+}
 }
 
-module.exports = { balanceParens }
+
+
+console.log(balanceParens("a(b)c)"))
+
+// module.exports = { balanceParens }
